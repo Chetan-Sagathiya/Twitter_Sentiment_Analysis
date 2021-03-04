7c49e654c457e8b076861653a67cd298e37db3ea
ChetanSagathiya <chetuu.018@gmail.com>
2021-03-04 19:58:26 +0530

    manipulating user tweet from website

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
 from flask import Flask, render_template, request
+import sentiment_analysis
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
 	return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+	sentiment = ''
+	if request.method == 'POST':
+		tweet = request.form['tweet']
+		tweet = sentiment_analysis.clean_tweets(tweet)
+		return render_template('result.html', sentiment = tweet)
